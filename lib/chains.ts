b7234d1fc36e6e72431e09788293f276b278e0cd
Talles Borges <talles.borges92@gmail.com>
2025-11-05 08:28:32 -0300

    Update chain URLs to use archive endpoints

--- lib/chains.ts
+++ lib/chains.ts
@@ -3,12 +3,12 @@ import { matrix, matrixBlockchain } from "@polkadot-api/descriptors";
 export const CHAINS = {
   canary: {
     name: "canary-matrixchain",
-    url: "wss://rpc.matrix.canary.enjin.io",
+    url: "wss://archive.matrix.canary.enjin.io",
     descriptor: matrix,
   },
   matrix: {
     name: "matrix-blockchain",
-    url: "wss://rpc.matrix.blockchain.enjin.io",
+    url: "wss://archive.matrix.blockchain.enjin.io",
     descriptor: matrixBlockchain,
   },
 } as const;
